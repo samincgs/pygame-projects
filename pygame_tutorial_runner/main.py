460f68a8d6a42f20618b12e64a47767e8bfd0821
samin <samincgs@gmail.com>
2023-09-05 23:51:06 -0400

    initialized a basic setup in pygame

--- pygame_tutorial_runner/main.py
+++ pygame_tutorial_runner/main.py
@@ -1,1 +1,24 @@
-import pygame
+import pygame, sys
+
+pygame.init() 
+
+width = 800
+height = 400
+fps = 60
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Tutorial Runner")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    # draw all our elements
+    # update everything
+    
+    
+    
+    pygame.display.update()
+    clock.tick(fps)
