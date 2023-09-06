59ae80508c8199553c690a51847d0ac040257922
samin <samincgs@gmail.com>
2023-09-06 19:09:04 -0400

    Learned more about surfaces in pygame. Also learned how to utilize these surfaces in order to display text, images and shapes

--- pygame_tutorial_runner/main.py
+++ pygame_tutorial_runner/main.py
@@ -9,15 +9,21 @@ fps = 60
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tutorial Runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+
+sky_surface = pygame.image.load("graphics/Sky.png")
+ground_surface = pygame.image.load("graphics/ground.png")
+text_surface = test_font.render("My game", False, "black"  )
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    # draw all our elements
-    # update everything
-    
+   
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
     
     
     pygame.display.update()
