b8029a58da13faa77c38182d9d353decb5ec74dd
samin <samincgs@gmail.com>
2023-09-06 20:07:00 -0400

    displayed the sky,ground, text and the snail images on to the display screen and made basic animations where the snail moves in a loop

--- pygame_tutorial_runner/main.py
+++ pygame_tutorial_runner/main.py
@@ -11,9 +11,12 @@ pygame.display.set_caption("Tutorial Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load("graphics/Sky.png")
-ground_surface = pygame.image.load("graphics/ground.png")
+sky_surface = pygame.image.load("graphics/Sky.png").convert()
+ground_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("My game", False, "black"  )
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -24,7 +27,10 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    
+    snail_x_pos-=3
+    if snail_x_pos + snail_surface.get_width() < 0:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 250))
     
     pygame.display.update()
     clock.tick(fps)
