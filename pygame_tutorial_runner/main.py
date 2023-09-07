6d35e26ea8b9689ea85c2a00af0af54269a62b1d
samin <samincgs@gmail.com>
2023-09-07 13:36:20 -0400

    learned how to get keyboard input. Can now check for key events in the event loop or in the game itself

--- pygame_tutorial_runner/main.py
+++ pygame_tutorial_runner/main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 
-pygame.init() 
+pygame.init()
 
 width = 800
 height = 400
@@ -14,14 +14,14 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 
-score_surf = test_font.render("Score: ", False, (64,64,64)  )
-score_rect = score_surf.get_rect(center = (width/2, height/2 - 150))
+score_surf = test_font.render("Score: ", False, (64, 64, 64))
+score_rect = score_surf.get_rect(center=(width / 2, height / 2 - 150))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_rect = snail_surf.get_rect(bottomleft = (600, 300))
+snail_rect = snail_surf.get_rect(bottomleft=(600, 300))
 
 player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
-player_rect = player_surf.get_rect(midbottom = (80, 300))
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -31,31 +31,41 @@ while True:
         # if event.type == pygame.MOUSEMOTION:
         #     if player_rect.collidepoint(event.pos):
         #         print("hit")
-    
-    
-    
-   
-    screen.blit(sky_surface, (0,0))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("jump")
+        if event.type == pygame.KEYUP:
+            print("key up")
+
+    screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    pygame.draw.rect(screen, 	(192, 232, 236), score_rect, )
-    pygame.draw.rect(screen, 	(192, 232, 236), score_rect, 10)
+    pygame.draw.rect(
+        screen,
+        (192, 232, 236),
+        score_rect,
+    )
+    pygame.draw.rect(screen, (192, 232, 236), score_rect, 10)
     screen.blit(score_surf, score_rect)
-    
+
     # pygame.draw.line(screen, "black", (0, 0), (800,400))
     # pygame.draw.ellipse(screen, "aqua", pygame.Rect(50, 200, 20, 20))
-    
-    
+
     screen.blit(snail_surf, snail_rect)
-    snail_rect.x -=3
-    if snail_rect.right < 0: snail_rect.left = 800
-    screen.blit(player_surf, player_rect )
-    
-    if player_rect.colliderect(snail_rect):
-        print("collision")
-    
+    snail_rect.x -= 3
+    if snail_rect.right < 0:
+        snail_rect.left = 800
+    screen.blit(player_surf, player_rect)
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     print("jump")
+
+    # if player_rect.colliderect(snail_rect):
+    #     print("collision")
+
     # mouse_pos = pygame.mouse.get_pos()
     # if player_rect.collidepoint((mouse_pos)):
     #     print("hit")
-    
+
     pygame.display.update()
     clock.tick(60)
