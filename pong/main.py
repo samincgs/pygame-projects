811677102c73a3739aad48c477a0f0425072d96e
samin <samincgs@gmail.com>
2023-09-11 02:14:56 -0400

    made the ball animation where it can bounce of the balls and created collisions for the player and opponents. Also added a small ai for opponent to track the ball and move according to it

--- pong/main.py
+++ pong/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 
+
 pygame.init()
 
 width, height = 1280, 960
@@ -11,8 +12,45 @@ pygame.display.set_caption("Pong")
 clock = pygame.time.Clock()
 
 ball = pygame.Rect(width / 2 - 15, height / 2 - 15, 30, 30)
+ball_speed_x = 7
+ball_speed_y = 7
+
 player = pygame.Rect(width - 20, height / 2 - 70, 10, 140)
+player_speed = 7
+
 opponent = pygame.Rect(10, height / 2 - 70, 10, 140)
+opponent_speed = 7
+
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.bottom >= height or ball.top <= 0:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+
+def player_input():
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_DOWN] and player.bottom <= height:
+        player.y += player_speed
+    if keys[pygame.K_UP] and player.top >= 0:
+        player.y -= player_speed
+
+
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.y += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.y -= opponent_speed
 
 
 while True:
@@ -20,6 +58,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    ball_animation()
+    player_input()
+    opponent_ai()
 
     screen.fill(bg_color)
 
