a6fd4a12f3e4998381d8f1acf31fe7013f228288
samin <samincgs@gmail.com>
2023-09-10 21:16:40 -0400

    created the basic setup in pygame

--- pong/main.py
+++ pong/main.py
@@ -1,1 +1,18 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+
+width, height = 1280, 960
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pong")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
