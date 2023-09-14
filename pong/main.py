4aeeed28c7ccc81599e615197d9f7824cc0d81dc
Samin <samincgs@gmail.com>
2023-09-14 15:28:19 -0400

    added extra collision to the paddles to ensure that the ball does not break when coming in contact with the edges of the paddle

--- pong/main.py
+++ pong/main.py
@@ -44,9 +44,20 @@ def ball_animation():
         score_time = pygame.time.get_ticks()
 
     if ball.colliderect(player) and ball_speed_x > 0:
-        ball_speed_x *= -1
+        if abs(ball.right - player.left) < 10:
+            ball_speed_x *= -1
+        elif abs(ball.top - player.bottom) < 10 and ball_speed_y < 0:
+            ball_speed_y *= -1
+        elif abs(ball.bottom - player.top) < 10 and ball_speed_y > 0:
+            ball_speed_y *= -1
+
     if ball.colliderect(opponent) and ball_speed_x < 0:
-        ball_speed_x *= -1
+        if abs(ball.left - opponent.right) < 10:
+            ball_speed_x *= -1
+        elif abs(ball.top - opponent.bottom) < 10 and ball_speed_y < 0:
+            ball_speed_y *= -1
+        elif abs(ball.bottom - opponent.top) < 10 and ball_speed_y > 0:
+            ball_speed_y *= -1
 
 
 def player_input():
