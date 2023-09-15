b0eb4d381fbc2496de7e356f651d5604b7d0f590
Samin <samincgs@gmail.com>
2023-09-14 22:40:06 -0400

    imported all assets (target, crosshair and background) and created an aim trainer type game

--- extra_tutorials/sprites/main.py
+++ extra_tutorials/sprites/main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 
 class Crosshair(pygame.sprite.Sprite):
@@ -8,15 +8,24 @@ class Crosshair(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         # self.gunshot = pygame.mixer.Sound()
 
-    # def shoot(self):
+    def shoot(self):
+        pygame.sprite.spritecollide(crosshair, target_group, True)
 
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
 
 
+class Target(pygame.sprite.Sprite):
+    def __init__(self, picture_path, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.image.load(picture_path).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos_x, pos_y)
+
+
 pygame.init()
 
-screen_width, screen_height = 1120, 680
+screen_width, screen_height = 1200, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Aim Trainer")
 clock = pygame.time.Clock()
@@ -31,6 +40,15 @@ crosshair = Crosshair("images/crosshair.png")
 crosshair_group = pygame.sprite.Group()
 crosshair_group.add(crosshair)
 
+target_group = pygame.sprite.Group()
+
+for target in range(12):
+    new_target = Target(
+        "images/target_red2.png",
+        random.randrange(0, screen_width),
+        random.randrange(0, screen_height),
+    )
+    target_group.add(new_target)
 
 while True:
     for event in pygame.event.get():
@@ -38,9 +56,15 @@ while True:
             pygame.quit()
             sys.exit()
 
+        mouse = pygame.mouse.get_pressed()
+        if mouse[0]:
+            crosshair.shoot()
+
     screen.blit(background, (0, 0))
 
+    target_group.draw(screen)
     crosshair_group.draw(screen)
+
     crosshair_group.update()
 
     pygame.display.update()
