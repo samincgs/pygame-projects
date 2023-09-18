0a49c285f08f5e1b46da939a1e0f3f459bfc3f02
samin <samincgs@gmail.com>
2023-09-18 19:48:20 -0400

    created laser logic and now spaceship creates laser through mouseclick

--- asteroid_shooter/main_class.py
+++ asteroid_shooter/main_class.py
@@ -22,9 +22,10 @@ class Ship(pygame.sprite.Sprite):
     def laser_shoot(self):
         mouse_input = pygame.mouse.get_pressed()[0]
         if mouse_input and self.can_shoot:
-            print("shoot")
             self.can_shoot = False
             self.shoot_time = pygame.time.get_ticks()
+
+            Laser(laser_group, self.rect.midtop)
 
     def update(self):
         self.laser_time()
@@ -34,10 +35,17 @@ class Ship(pygame.sprite.Sprite):
 
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, groups, pos_x, pos_y):
+    def __init__(self, groups, pos):
         super().__init__(groups)
         self.image = pygame.image.load("graphics/laser.png").convert_alpha()
-        self.rect = self.image.get_rect(midbottom=(pos_x, pos_y))
+        self.rect = self.image.get_rect(midbottom=(pos))
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(0, -1)
+        self.speed = 600
+
+    def update(self):
+        self.pos += self.direction * self.speed * dt
+        self.rect.topleft = (round(self.pos.x), round(self.pos.y))
 
 
 pygame.init()
@@ -55,7 +63,7 @@ ship_group = pygame.sprite.GroupSingle()
 laser_group = pygame.sprite.Group()
 
 ship = Ship(ship_group)
-laser = Laser(laser_group, 200, 60)
+laser = Laser(laser_group, (200, 600))
 
 
 while True:
@@ -72,5 +80,6 @@ while True:
     laser_group.draw(screen)
 
     ship_group.update()
+    laser_group.update()
 
     pygame.display.update()
