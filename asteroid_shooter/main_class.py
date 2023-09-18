10b4d483d38b387f407c39c2db5944406118a906
samin <samincgs@gmail.com>
2023-09-18 18:12:37 -0400

    created spaceship movement and laser shooting and timing mechanics in the class version of the game

--- asteroid_shooter/main_class.py
+++ asteroid_shooter/main_class.py
@@ -6,13 +6,38 @@ class Ship(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load("graphics/ship.png").convert_alpha()
         self.rect = self.image.get_rect(center=(screen_width / 2, screen_height / 2))
+        self.can_shoot = True
+        self.shoot_time = None
+
+    def input_position(self):
+        mouse_pos = pygame.mouse.get_pos()
+        self.rect.center = mouse_pos
+
+    def laser_time(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time > 500:
+                self.can_shoot = True
+
+    def laser_shoot(self):
+        mouse_input = pygame.mouse.get_pressed()[0]
+        if mouse_input and self.can_shoot:
+            print("shoot")
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def update(self):
+        self.laser_time()
+        self.laser_shoot()
+        self.input_position()
+        self.laser_shoot()
 
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, groups, pos_x, pos_y):
         super().__init__(groups)
         self.image = pygame.image.load("graphics/laser.png").convert_alpha()
-        self.rect = self.image.get_rect(center=(pos_x, pos_y))
+        self.rect = self.image.get_rect(midbottom=(pos_x, pos_y))
 
 
 pygame.init()
@@ -27,9 +52,9 @@ bg = pygame.image.load("graphics/background.png").convert()
 
 # object calls
 ship_group = pygame.sprite.GroupSingle()
-ship = Ship(ship_group)
-
 laser_group = pygame.sprite.Group()
+
+ship = Ship(ship_group)
 laser = Laser(laser_group, 200, 60)
 
 
@@ -45,5 +70,7 @@ while True:
 
     ship_group.draw(screen)
     laser_group.draw(screen)
+
+    ship_group.update()
 
     pygame.display.update()
